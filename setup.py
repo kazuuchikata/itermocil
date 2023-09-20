895e64af55ff784103f764dbd44adbabc070e41b
Kazuhiro Uchikata <kazuuchikata@gmail.com>
2023-09-19 22:47:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='itermocil',
-    version='1.0.3',
+    version='1.0.4',
     url='https://github.com/TomAnthony/itermocil',
     license='MIT',
     description='Create pre-defined window/pane layouts and run commands in iTerm',
